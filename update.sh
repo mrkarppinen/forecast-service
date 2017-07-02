bf9c7ae27932312cc4bd01c7ad97ea448e17dafb
jussi <mrkarppinen@gmail.com>
2017-07-02 23:42:21 +0300

    update script updated

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ rm *.zip
 zip -r forecast.zip package.json index.js src node_modules
 
 #wsk action create forecast2 --param-file parameters.json --kind nodejs:6 forecast.zip
-wsk action update forecast2 --param-file parameters.json --kind nodejs:6 forecast.zip
+wsk action update forecast2 --web true --param-file parameters.json --kind nodejs:6 forecast.zip
