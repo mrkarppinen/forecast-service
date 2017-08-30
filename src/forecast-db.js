99489dd2cee20d00154b4f80017645e8a39789c6
jussi <mrkarppinen@gmail.com>
2017-08-30 18:41:14 +0300

    refactoring promises

--- src/forecast-db.js
+++ src/forecast-db.js
@@ -8,51 +8,43 @@ class ForecastDB {
 
   get(location){
 
-      return new Promise( (resolve, reject) => {
-          this.db.get(location)
-          .then( (body) => { resolve(body); } )
-          .catch( (error) => {
+     return  this.db.get(location)
+        .then( (body) => body )
+        .catch( (error) => {
+          if (error.statusCode == 404) {
+            return null;
+          }else {
+            return error;
+          }
 
-            if (error.statusCode == 404) {
-              resolve(null);
-            }else {
-              reject(error);
-            }
-
-           } );
-      });
+        } );
 
   }
 
 
   insert(location, data){
-      return new Promise( (resolve, reject) => {
+
         const updated = new Date().getTime();
-        this.db.insert({updated:updated, data: data}, location).then(
+        return this.db.insert({updated:updated, data: data}, location).then(
           (doc) => { 
             doc.updated = updated;
             doc.data = data;
-            resolve(doc);
-          },
-          reject
+            return doc;
+          }
         );
 
 
-      });
   }
 
   update(id, rev, data){
-      return new Promise( (resolve, reject) => {
         const updated = new Date().getTime();
-        this.db.insert({_id: id, _rev:rev, updated:updated, data: data}).then(
+        return this.db.insert({_id: id, _rev:rev, updated:updated, data: data}).then(
           (doc) => {
             doc.updated = updated;
             doc.data = data;
-            resolve(doc);
-          },
-          reject
+            return doc;
+          }
         );
-      });
   }
 
   clear(){
